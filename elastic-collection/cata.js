bb84d9604dd2cd405796f29251004cc5064f1a54
Juany026 <juany026@users.noreply.github.com>
2022-05-13 15:27:57 -0400

    stuck at filter return

--- elastic-collection/cata.js
+++ elastic-collection/cata.js
@@ -6,17 +6,17 @@ fetch('https://api.airtable.com/v0/appmI7DNf6ICGvK0p/Table%201', {
   .then(res => res.json()) // tells JS to expect data in json format
   .then(data => {
 
-    data.records.filter(manga => {
-        return manga.publishedYear < 2000
-
-    })
 
     console.log(data); 
     
     const mangaContainer = document.querySelector('.content'); // tell JS about the div we added to our html file so we can put content inside it
 
-    data.records.forEach(manga => {
-        console.log(manga); // look in the console at each album to see what fields we can access (these are your own table headers from Airtable)
+    data
+      .filter(manga => {
+        return manga.fields.publishedYear < 2000
+      })
+      
+      .forEach(manga => {
 
         //albumsContainer.innerHTML += `
         //<div class="album">
@@ -27,19 +27,17 @@ fetch('https://api.airtable.com/v0/appmI7DNf6ICGvK0p/Table%201', {
         //</div>
         //`;
 
-        mangaContainer.innerHTML += `
-        <div class="manga">
-            <div class="cover-container">
-                <img src="${manga.fields.comicCover[0].thumbnails.large.url}">
-            </div>
-
-            <h4>${manga.fields.title}</h4>
-            <h5>${manga.fields.artistName}</h5>
-            <h6>${manga.fields.publishedYear}</h6>
-            <h2>${manga.fields.style}</h2>
-        </div>
-
-        `;
+          mangaContainer.innerHTML += `
+          <div class="manga">
+              <div class="cover-container">
+                  <img src="${manga.fields.comicCover[0].thumbnails.large.url}">
+              </div>
+              <h4>${manga.fields.title}</h4>
+              <h5>${manga.fields.artistName}</h5>
+              <h6>${manga.fields.publishedYear}</h6>
+              <h2>${manga.fields.style}</h2>
+          </div>
+          `;
 
 
 
